bb5eb1f76e028b4a85773117668ed0ebced9dc15
layelhajji <layelhajji3@gmail.com>
2024-08-15 00:12:33 +0100

    add employee front

--- client/emp_management/src/components/addEmployee/AddEmp.jsx
+++ client/emp_management/src/components/addEmployee/AddEmp.jsx
@@ -1,11 +1,128 @@
-
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './add.css';
 
 const AddEmp = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        EmployeeID: "",
+        fullName: "",
+        age: "",
+        phone: "",
+        joinedDate: ""
+    });
 
-export default AddEmp
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Form submitted!");
+
+        if (!formData.EmployeeID || !formData.fullName || !formData.age || !formData.phone || !formData.joinedDate) {
+            alert("All fields are required");
+            return;
+        }
+
+        try {
+            const response = await fetch("http://127.0.0.1:3000/api/add", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const result = await response.json();
+            console.log("Result:", result);
+
+            if (response.ok) {
+                alert("Employee added successfully");
+                navigate("/dashboard");
+            } else if (response.status === 409) {
+                alert("Employee already exists");
+            } else {
+                console.error("Failed to add:", result.message);
+            }
+        } catch (error) {
+            console.error("Error during submission:", error.message);
+        } finally {
+            setFormData({
+                EmployeeID: "",
+                fullName: "",
+                age: "",
+                phone: "",
+                joinedDate: ""
+            });
+        }
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    return (
+        <div className="formulaire">
+            <Form onSubmit={handleSubmit}>
+                <h1>Add Employee</h1>
+                <Form.Group className="mb-3" controlId="formEmployeeID">
+                    <Form.Label>Employee ID</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="EmployeeID"
+                        value={formData.EmployeeID}
+                        onChange={handleInputChange}
+                    />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="formFullName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleInputChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formAge">
+                    <Form.Label>Age</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="age"
+                        value={formData.age}
+                        onChange={handleInputChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formPhone">
+                    <Form.Label>Mobile Number</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleInputChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formJoinedDate">
+                    <Form.Label>Joined Date</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="joinedDate"
+                        value={formData.joinedDate}
+                        onChange={handleInputChange}
+                    />
+                </Form.Group>
+                
+                <Button className="btn custom-button" type="submit">
+                    Add
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default AddEmp;
--- server/src/controllers/employee.js
+++ server/src/controllers/employee.js
@@ -1,19 +1,22 @@
-const employeeService=require('../services/employee');
+const employeeService = require('../services/employee');
 
-
-async function createEmp(req,res) {
+async function createEmp(req, res) {
     try {
-        const EmployeeData=req.body
-        const savedEmployee=employeeService.createEmp(EmployeeData)
-        res.json(savedEmployee)
+        const EmployeeData = req.body;
+
         
+        const savedEmployee = await employeeService.createEmp(EmployeeData);
+
+        if (savedEmployee.success) {
+            res.status(200).json(savedEmployee);
+        } else {
+            res.status(409).json({ message: savedEmployee.message });
+        }
 
     } catch (error) {
-        console.log(error)
-        res.status(400).json({message:error.message})  
+        console.log(error);
+        res.status(400).json({ message: error.message });
     }
-    
 }
 
-
-module.exports={createEmp};
+module.exports = { createEmp };
